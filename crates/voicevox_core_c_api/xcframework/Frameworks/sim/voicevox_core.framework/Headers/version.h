4a8b48dfb91dcb1f32288b9153cf2a2546b106ce
nekomimimi <okt666@yahoo.co.jp>
2023-12-31 01:14:54 +0900

    xcframeworkでdylibをframeworkに入れる

---
+++ crates/voicevox_core_c_api/xcframework/Frameworks/sim/voicevox_core.framework/Headers/version.h
@@ -0,0 +1,18 @@
+//
+//  voicevox_core.h
+//  voicevox_core
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for voicevox_core.
+FOUNDATION_EXPORT double voicevox_coreVersionNumber;
+
+//! Project version string for voicevox_core.
+FOUNDATION_EXPORT const unsigned char voicevox_coreVersionString[];
+
+
+
+
+
+
---
+++ crates/voicevox_core_c_api/xcframework/README.md
@@ -0,0 +1,12 @@
+# xcframeworkフォルダの内容について
+## Frameworks
+
+iOS向けの配布ライブラリにXCFramework形式を採用しています。
+このXCFrameworkは、端末用（arm64）と
+シミュレータ用（arm64_x86_64-simulator）の2種類のFrameworkを
+含みます。
+それらのフレームワークを作るために使用されます。
+
+なお、この2つのframeworkはXcodeで空のframeworkを作成しました。
+
+
